c1ac28a2cb966a60d9fc91183f6137a9e2b4a2be
Ahmed Nagah <ahmednagah031220@gmail.com>
2024-02-28 14:54:42 +0200

    Header edit 0x01

--- shell.h
+++ shell.h
@@ -95,7 +95,7 @@ typedef struct passinfo
 	int histcount;
 } info_t;
 
-#define INFO_INIT
+#define INFO_INIT \
 {NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
 	0, 0, 0}
 
